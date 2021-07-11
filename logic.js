0e0c14e68b81e150147045fb7f8d0cf21d6ad93b
Rishav <75078593+mairishavhoon@users.noreply.github.com>
2021-07-11 19:24:31 +0530

    created the JS for the site UwU

---
+++ logic.js
@@ -0,0 +1,26 @@
+function kloc(){
+    var t = new Date();
+    var h = t.getHours();
+    var m = t.getMinutes();
+    var s = t.getSeconds();
+    var ap = "AM"
+    if(h>12){
+        ap = "PM"
+    }
+    if(h>12){
+        h = h-12;
+    }
+    if(h<10){
+        h = "0"+h;
+    }
+    if(m<10){
+        m = "0"+m;
+    }
+    if(s<10){
+        s = "0"+s;
+    }
+    document.getElementById("hr").innerHTML = h+" : ";
+    document.getElementById("mi").innerHTML = m+" : ";
+    document.getElementById("se").innerHTML = s;
+    document.getElementById("ap").innerHTML = ap;
+}
